033c44daa4edec7deccb6be34a3fea2cf5e79a00
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-09-23 03:49:20 +0300

    done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
+import React,{useState} from "react";
 
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  function increment() {
+function Counter(){
+   
+  const [count,setCount] = useState(0)
+  function increment(){
+    
     setCount(count + 1);
+    
+  setCount(count + 1);
+  console.log(count)
+  
   }
+  
+     return(
+      <button onClick={increment}>{count}</button>
+     )
+    }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
-}
 
 export default Counter;
